56cee53329ed255bb43fe16a425bc1d4e80e0370
AmrSayed74 <amrsaaayed74@gmail.com>
2023-07-02 20:52:07 +0300

    Updating Portfolio

--- js/main.js
+++ js/main.js
@@ -19,23 +19,27 @@ function preload() {
   });
 }
 
-preload();
+// preload();
 /* ===== End Preload ====== */
 
 /* ===== Start Links Header ====== */
-const headerLinks = document.querySelectorAll(".ul-nav .nav-list .nav-link");
-const navMenuIcon = document.querySelector(".menu-icon");
-const navPhoneMenu = document.getElementById("phone-nav");
+const headerLinks = document.querySelectorAll('.ul-nav .nav-list .nav-link');
+const navMenuIcon = document.querySelector('.menu-icon');
+const navPhoneMenu = document.getElementById('phone-nav');
+const contactBtn = document.querySelector('.c-button');
+const contactModal = document.getElementById('contact');
+const contactClose = document.querySelector('.contact-close__img');
+
 const navPhoneLinks = document.querySelectorAll(
-  ".phone-nav-ul .phone-nav-li .phone-link"
+  '.phone-nav-ul .phone-nav-li .phone-link'
 );
 
 function scrollingView(elements) {
   elements.forEach((ele) => {
-    ele.addEventListener("click", (e) => {
+    ele.addEventListener('click', (e) => {
       e.preventDefault();
       document.querySelector(e.target.dataset.section).scrollIntoView({
-        behavior: "smooth",
+        behavior: 'smooth',
       });
     });
   });
@@ -44,67 +48,111 @@ scrollingView(headerLinks);
 scrollingView(navPhoneLinks);
 
 function toggleClass(active, show) {
-  active.classList.toggle("active");
-  show.classList.toggle("show");
+  active.classList.toggle('active');
+  show.classList.toggle('show');
 }
 
-if (document.querySelector(".menu-icon") != null) {
-  navMenuIcon.addEventListener("click", (e) => {
+if (document.querySelector('.menu-icon') != null) {
+  navMenuIcon.addEventListener('click', (e) => {
     toggleClass(navMenuIcon, navPhoneMenu);
   });
 }
 navPhoneLinks.forEach((link) => {
-  link.addEventListener("click", () => {
+  link.addEventListener('click', () => {
     toggleClass(navMenuIcon, navPhoneMenu);
   });
+});
+
+contactBtn.addEventListener('click', () => {
+  contactModal.classList.add('active-contact');
+});
+contactClose.addEventListener('click', () => {
+  contactModal.classList.remove('active-contact');
 });
 
 /* ===== End Links Header ====== */
 
 /* ===== Start Home Section Scroll Button ====== */
-const scrollToAboutSection = document.querySelector(".scrolldown");
-if (document.querySelector(".scrolldown") != null) {
-  scrollToAboutSection.addEventListener("click", (e) => {
+const scrollToAboutSection = document.querySelector('.scrolldown');
+if (document.querySelector('.scrolldown') != null) {
+  scrollToAboutSection.addEventListener('click', (e) => {
     e.preventDefault();
     document.querySelector(e.target.dataset.section).scrollIntoView({
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   });
 }
 /* ===== End Home Section Scroll Button ====== */
 /* ===== Start TypeWriter Effect  ====== */
-if (document.querySelector(".typed") != null) {
-  let typed = new Typed(".typed", {
-    strings: [
-      "UI Developer",
-      "Freelancer",
-      " Front-End Developer",
-      "Articles Writer",
-      "Content Creator",
-    ],
-    typeSpeed: 100,
-    backSpeed: 50,
-    backDelay: 2000,
-    loop: true,
-  });
-  console.log(typed);
+if (document.querySelector('#typewriter') != null) {
+  // let typed = new typed(".typed", {
+  //   strings: [
+  //     "UI Developer",
+  //     "Freelancer",
+  //     " Front-End Developer",
+  //     "Articles Writer",
+  //     "Content Creator",
+  //   ],
+  //   typeSpeed: 100,
+  //   backSpeed: 50,
+  //   backDelay: 2000,
+  //   loop: true,
+  // });
+
+  const words = [
+    'UI Developer',
+    'Freelancer',
+    'Front-End Developer',
+    'Articles Writer',
+    'Content Creator',
+  ];
+
+  const typewriter = document.getElementById('typewriter');
+  let wordIndex = 0;
+  let charIndex = 0;
+
+  function type() {
+    if (charIndex < words[wordIndex].length) {
+      typewriter.textContent += words[wordIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(type, 100); // Adjust typing speed here
+    } else {
+      setTimeout(erase, 2000); // Adjust pause before erasing here
+    }
+  }
+
+  function erase() {
+    if (charIndex >= 0) {
+      typewriter.textContent = words[wordIndex].substring(0, charIndex);
+      charIndex--;
+      setTimeout(erase, 50); // Adjust erasing speed here
+    } else {
+      wordIndex++;
+      if (wordIndex >= words.length) {
+        wordIndex = 0; // Start from the beginning after cycling through all words
+      }
+      setTimeout(type, 500); // Adjust pause before typing next word here
+    }
+  }
+
+  type(); // Start the typewriter effect
 }
 /* ===== End TypeWriter Effect  ====== */
 
 /* ===== Start Scroll To Top Button ===== */
-const scrollTop = document.querySelector(".scroll-top");
-if (document.querySelector(".scroll-top") != null) {
-  window.addEventListener("scroll", () => {
+const scrollTop = document.querySelector('.scroll-top');
+if (document.querySelector('.scroll-top') != null) {
+  window.addEventListener('scroll', () => {
     if (window.scrollY > 600) {
-      scrollTop.classList.add("active");
+      scrollTop.classList.add('active');
     } else {
-      scrollTop.classList.remove("active");
+      scrollTop.classList.remove('active');
     }
   });
-  scrollTop.addEventListener("click", () => {
+  scrollTop.addEventListener('click', () => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   });
 }
